0573fab7787b449d398fee7bf5fc64ab7bc81461
ariverapaniza <ariverapaniza@gmail.com>
2024-04-18 07:00:24 +1000

    updated director's profile

--- director_profile.php
+++ director_profile.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Directors Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -21,11 +22,31 @@
     $director = $directorResult->fetch_assoc();
 
     if ($director) {
-        echo "<div class='container mt-5'>";
-        echo "<h1>" . htmlspecialchars($director['fname']) . " " . htmlspecialchars($director['lname']) . "</h1>";
-        echo "<img src='" . htmlspecialchars($director['photo']) . "' class='img-fluid' alt='Director Image'>";
-        echo "<p>" . htmlspecialchars($director['description']) . "</p>";
-        echo "</div>";
+    ?>
+    <div class="container mt-5">
+        <h1 class="text-center text-white"><?= htmlspecialchars($director['fname']) ?>
+            <?= htmlspecialchars($director['lname']) ?></h1>
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4 d-flex justify-content-center">
+                <img src="<?= htmlspecialchars($director['photo']) ?>" class="img-fluid rounded" alt="Director Image">
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="table-responsive">
+                <table class="table table-dark table-striped table-bordered w-100">
+                    <tbody>
+                        <tr>
+                            <th>Description</th>
+                        </tr>
+                        <tr>
+                            <td><?= htmlspecialchars($director['description']) ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <?php
     } else {
         echo "<p>Director not found.</p>";
     }
